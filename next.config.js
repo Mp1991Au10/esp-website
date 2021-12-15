f30c8aab76974cf62f4b3bca9d2d876a66876732
Nicolás Quiroz <nh.quiroz@gmail.com>
2021-12-15 11:28:57 -0300

    chore: update form to use API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  env: {
-    WEB_TO_LEAD_BASE_URL: process.env.WEB_TO_LEAD_BASE_URL
-  }
+  reactStrictMode: true
 };
