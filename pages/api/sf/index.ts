ec1e836ab15b170588946d11b2c9abef6d7db492
Nicolás Quiroz <nh.quiroz@gmail.com>
2021-12-15 13:30:17 -0300

    chore: update /api/sf

--- pages/api/sf/index.ts
+++ pages/api/sf/index.ts
@@ -1,10 +1,11 @@
-import Cors from 'cors';
+// import Cors from 'cors';
+import jsforce from 'jsforce';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const cors = Cors({
-  methods: ['HEAD', 'POST'],
-  origin: '*'
-});
+// const cors = Cors({
+//   methods: ['HEAD', 'POST'],
+//   origin: '*'
+// });
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -21,27 +22,53 @@ const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: any) =>
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Run the middleware
-  await runMiddleware(req, res, cors);
+  // await runMiddleware(req, res, cors);
 
-  // Rest of the API logic
   const { body } = req;
-  const { oid, retURL, first_name, email, company, description, _00N5J000000QWsP } =
-    JSON.parse(body);
+  const { first_name, last_name, email, company, recordType } = body;
+  const {
+    SF_CONNECTION_LOGIN_URL,
+    SF_CONNECTION_USERNAME,
+    SF_CONNECTION_PASSWORD,
+    SF_CONNECTION_SECURITY_TOKEN
+  } = process.env;
 
-  const QUERY_PARAMS = `&oid=${oid}&retURL=${retURL}&first_name=${first_name}&email=${email}&company=${company}&00N5J000000QWsP=${_00N5J000000QWsP}&description=${description}`;
+  const conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl: SF_CONNECTION_LOGIN_URL
+  });
+  conn.login(
+    SF_CONNECTION_USERNAME!,
+    `${SF_CONNECTION_PASSWORD}${SF_CONNECTION_SECURITY_TOKEN}`,
+    (err, userInfo) => {
+      if (err) {
+        return console.error(err);
+      }
+      // Now you can get the access token and instance URL information.
+      // Save them to establish connection next time.
+      console.log(conn.accessToken);
+      console.log(conn.instanceUrl);
+      // logged in user property
+      console.log('User ID: ' + userInfo.id);
+      console.log('Org ID: ' + userInfo.organizationId);
 
-  console.log({ QUERY_PARAMS });
-
-  return fetch(`${process.env.WEB_TO_LEAD_BASE_URL}${QUERY_PARAMS}`, {
-    method: 'POST',
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    mode: 'no-cors',
-    body
-  })
-    .then(_ => {
-      res.status(200).json({ status: 'ok' });
-    })
-    .catch(console.error);
+      // Single record creation
+      conn.sobject('Lead').create(
+        {
+          LastName: last_name,
+          Email: email,
+          Company: company,
+          RecordTypeId: recordType
+        },
+        (err, ret) => {
+          if (err || !ret.success) {
+            console.error({ err });
+            res.status(400).json({ status: 'fail' });
+          }
+          console.log('SUCCESS!');
+          res.status(200).json({ status: 'ok' });
+        }
+      );
+    }
+  );
 }
