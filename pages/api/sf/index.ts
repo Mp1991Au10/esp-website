72fed5dd8ebfd25cb3f7db5ef588a45b7403711f
Nicolás Quiroz <nh.quiroz@gmail.com>
2021-12-16 13:32:31 -0300

    feat: use access token for request auth

--- pages/api/sf/index.ts
+++ pages/api/sf/index.ts
@@ -1,53 +1,54 @@
 import jsforce from 'jsforce';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { RecordTypeMap, RequestBody } from '../../../types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body } = req;
-  const { last_name: LastName, email: Email, company: Company, recordType: RecordTypeId } = body;
+  const { last_name: LastName, email: Email, company: Company, recordTypeFlag }: RequestBody = body;
   const {
-    SF_CONNECTION_LOGIN_URL,
-    SF_CONNECTION_USERNAME,
-    SF_CONNECTION_PASSWORD,
-    SF_CONNECTION_SECURITY_TOKEN
+    SF_CLIENT_ID,
+    SF_CLIENT_SECRET,
+    SF_REDIRECT_URL,
+    SF_INSTANCE_URL,
+    SF_ACCESS_TOKEN,
+    SF_REFRESH_TOKEN,
+    SF_RECORD_TYPE_ALPHA,
+    SF_RECORD_TYPE_BETA
   } = process.env;
 
-  const conn = new jsforce.Connection({
-    // you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl: SF_CONNECTION_LOGIN_URL
-  });
-  conn.login(
-    SF_CONNECTION_USERNAME!,
-    `${SF_CONNECTION_PASSWORD}${SF_CONNECTION_SECURITY_TOKEN}`,
-    (err, userInfo) => {
-      if (err) {
-        return console.error(err);
-      }
-      // Now you can get the access token and instance URL information.
-      // Save them to establish connection next time.
-      console.log(conn.accessToken);
-      console.log(conn.instanceUrl);
-      // logged in user property
-      console.log('User ID: ' + userInfo.id);
-      console.log('Org ID: ' + userInfo.organizationId);
+  const recordTypeMap: RecordTypeMap = {
+    alpha: SF_RECORD_TYPE_ALPHA!,
+    beta: SF_RECORD_TYPE_BETA!
+  };
 
-      // Single record creation
-      conn.sobject('Lead').create(
-        {
-          // LastName is the only required field by SF
-          LastName,
-          Email,
-          Company,
-          RecordTypeId
-        },
-        (err, ret) => {
-          if (err || !ret.success) {
-            console.error({ err });
-            res.status(400).json({ status: 'fail' });
-          }
-          console.log('SUCCESS!');
-          res.status(200).json({ status: 'ok' });
-        }
-      );
+  const conn = new jsforce.Connection({
+    oauth2: {
+      clientId: SF_CLIENT_ID,
+      clientSecret: SF_CLIENT_SECRET,
+      redirectUri: SF_REDIRECT_URL
+    },
+    instanceUrl: SF_INSTANCE_URL,
+    accessToken: SF_ACCESS_TOKEN,
+    // if refresh token is provided, the connection will automatically refresh the access token when it has expired
+    refreshToken: SF_REFRESH_TOKEN
+  });
+
+  // Single record creation
+  conn.sobject('Lead').create(
+    {
+      // LastName is the only required field by SF
+      LastName,
+      Email,
+      Company,
+      RecordTypeId: recordTypeMap[recordTypeFlag]
+    },
+    (err, ret) => {
+      if (err || !ret.success) {
+        console.error({ err });
+        res.status(400).json({ status: 'fail' });
+      }
+      console.log('SUCCESS!');
+      res.status(200).json({ status: 'ok' });
     }
   );
 }
